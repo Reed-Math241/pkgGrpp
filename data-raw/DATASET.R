970558d8f96a05b13848f57934a1c2f9e3c984f8
AnanananC <caoa@reed.edu>
2021-03-01 18:56:46 -0800

    1st Draft (Wrangling Done)

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,27 +5,22 @@ MD_crime <- readr::read_csv("Crime (2).csv")
 library(tidyverse)
 library(dplyr)
 library(stringr)
-MD_crime %>%
-  group_by(`Crime Name1`) %>%
-  count()
 
-MD_crime %>%
-  group_by(`Crime Name2`) %>%
-  count() %>%
-  as.data.frame()
-
-MD_crime %>%
-  group_by(`Crime Name3`) %>%
-  count() %>%
-  as.data.frame()
 
 MD_Crime1 <- MD_crime %>%
   filter(State == "MD") %>%
+  filter(grepl("2020", `Dispatch Date / Time`)) %>%
   select(`Incident ID`, `Dispatch Date / Time`, `Crime Name1`, `Crime Name2`, `Crime Name3`, 
          Victims, City,Place, Start_Date_Time, End_Date_Time,Longitude, Latitude) %>%
+  separate(col = `Dispatch Date / Time`, into = c("Dispatch Date", "Dispatch Time"), 
+           sep = " ") %>%
+  separate(col = End_Date_Time, into = c("End Date", "End Time"), 
+           sep = " ") %>%
+  separate(col = Start_Date_Time, into = c("Start Date", "Start Time"), 
+           sep = " ") %>%
   group_by(`Crime Name3`) %>%
   rename("CrimeName3" = `Crime Name3`) %>%
-  separate(col = CrimeName3, into = c("Crime Name3", "Crime Name4", "Crime Name 5"), 
+  separate(col = CrimeName3, into = c("Crime Name3", "Crime Name4", "Crime Name5"), 
            sep = "-") %>%
   separate(col = Place, into = c("Place1", "Place2"), 
            sep = "-") %>%
@@ -33,18 +28,38 @@ MD_Crime1 <- MD_crime %>%
 
 
 CN3 <- MD_Crime1$`Crime Name3`
-str_to_title(CN3) 
+cn3 <- str_to_title(CN3) 
 
 CN4 <- MD_Crime1$`Crime Name4`
-str_to_title(CN4) 
+cn4 <- str_to_title(CN4) 
 
-MD_Crime1 %>%
-  group_by(`Crime Name3`) %>%
-  count()
+CN5 <- MD_Crime1$`Crime Name5`
+cn5 <- str_to_title(CN5) 
 
-MD_Crime2 <- MD_Crime1 %>%
-  filter(`Crime Name1` == "Not a Crime" | `Crime Name1`== "Other")
+MD_Crime <- cbind(MD_Crime1, cn3, cn4, cn5) %>%
+  mutate(`Crime Name3` = cn3,
+         `Crime Name4` = cn4,
+         `Crime Name5` = cn5) %>%
+  select(!cn3) %>%
+  select(!cn4) %>%
+  select(!cn5) %>%
+  select(!`Crime Name2`) %>%
+  filter(!grepl("Describe Offense", `Crime Name3`)) 
 
-usethis::use_data(DATASET, overwrite = TRUE)
+MD_Crime2 <- gsub(' Non', 'Non-familiy', MD_Crime$`Crime Name5`)
+MD_Crime0 <- cbind(MD_Crime, MD_Crime2) %>%
+  mutate(`Crime Name5` = MD_Crime2) %>%
+  select(!MD_Crime2) %>%
+  rename(`Crime Name2` = `Crime Name3`,
+         `Crime Name3` = `Crime Name4`,
+         `Crime Name4` = `Crime Name5`)
+
+col_order <- c("Incident ID", "Crime Name1", "Crime Name2",
+               "Crime Name3", "Crime Name4", "Victims", "City", "Place1", "Place2",
+               "Dispatch Date", "Dispatch Time", "Start Date", "Start Time", "End Date", "End Time",
+               "Longitude", "Latitude")
+MD_Crime <- MD_Crime0[, col_order]
+
+usethis::use_data(MD_Crime, overwrite = TRUE)
 
 
